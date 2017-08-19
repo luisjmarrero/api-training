3e01b402d635cec3beb20ff2b0e9e1b0c93a0e32
Luis Marrero <luis124ma@gmail.com>
2017-08-19 17:42:23 -0400

    security does not work!

---
+++ restful-service/src/main/java/marrero/config/WebSecurityConfig.java
@@ -0,0 +1,31 @@
+package marrero.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled=true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().and().authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/people").hasRole("admin")
+                .antMatchers(HttpMethod.GET, "/people/**").hasRole("admin")
+                .and().csrf().disable();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("admin");
+        auth.inMemoryAuthentication().withUser("user").password("user").roles("user");
+    }
+
+}
